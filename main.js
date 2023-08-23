df19ebc014a25706df7a567d2868321bf89ca0f4
Perkles <anisio.gustavo@outlook.com>
2023-08-23 20:28:54 -0300

    atualizar

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-function tocaSom(idElementAudio){
-   document.querySelector(idElementAudio).play();
-}
+function tocaSom(seletorAudio){
+   const elemento = document.querySelector(seletorAudio);
+
+
+      if (elemento && elemento.localName === 'audio') {
+         elemento.play();
+      }
+      else {
+         console.log('Elemento nao encontrado');
+      }
+   }
 const ListaDeTeclas = document.querySelectorAll(".tecla");
 
 for(i = 0; i < ListaDeTeclas.length; i++){
-ListaDeTeclas[i].onclick = tocaSom;
+
+   const tecla = ListaDeTeclas[i];
+   const instrumento = tecla.classList[1];
+   const idAudio = `#som_${instrumento}`;
+
+   tecla.onclick = function() {
+      tocaSom(idAudio);
+   }
+
+   tecla.onkeydown = function() {
+      tecla.classList.add('ativa');
+   }
+   tecla.onkeyup = function() {
+      tecla.classList.remove('ativa');
+   }
 }
 
 
